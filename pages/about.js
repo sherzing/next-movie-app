d1f38e8d5fb72e7edb6965a34058bd3d4948448a
Sven <sven.herzing@gmail.com>
2020-02-03 23:38:17 +0400

    about as class comopnents

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-const About = () => {
-    const page = 'foo'
-    return (
-        <h1>Helle About {page}</h1>
-    )
+// when to use:
+// 1. for smaller components
+// 2. reusable components
+// 3. presentational components, partially right, we can use HOOKS and specify state
+// const About = () => {
+//     const message = 'foo'
+//     return (
+//         <h1>Helle About {message}</h1>
+//     )
+// }
+
+// when to use:
+// 1. bigger components
+// 2. lots of state
+class About extends React.Component {
+    render() {
+        return (
+            <h1>Hello I am class component</h1>
+        )
+    }
 }
+
 
 export default About
