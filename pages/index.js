8656866433a37802b616b7e54821330b40dcba4d
Sven Herzing <sven.herzing@careem.com>
2020-02-04 14:56:08 +0400

    fix style on index

--- pages/index.js
+++ pages/index.js
@@ -17,25 +17,29 @@ const Home = () => (
     </Head>
 
     <Navbar />
-
-    <div className="container">
-      <div className="row">
-
-        <div className="col-lg-3">
-          <SideMenu />
-        </div>
-
-        <div className="col-lg-9">
-          <Carousel />
-
-          <div className="row">
-
-            <MovieList />
+    <div className="home-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <SideMenu />
+          </div>
+          <div className="col-lg-9">
+            <Carousel />
+            <div className="row">
+              <MovieList />
+            </div>
           </div>
         </div>
       </div>
     </div>
     <Footer />
+
+    <style jsx>{`
+      .home-page {
+        padding-top: 80px;
+      }
+    `}
+    </style>
 
   </div>
 )
