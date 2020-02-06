104425cb3b20a13bb4f7a36f569391bf63fd0a60
Sven <sven.herzing@gmail.com>
2020-02-06 22:18:43 +0400

    add links in navbar

--- pages/about.js
+++ pages/about.js
@@ -4,23 +4,23 @@ import React from 'react'
 // 1. for smaller components
 // 2. reusable components
 // 3. presentational components, partially right, we can use HOOKS and specify state
-// const About = () => {
-//     const message = 'foo'
-//     return (
-//         <h1>Helle About {message}</h1>
-//     )
-// }
+const About = () => {
+    const message = 'foo'
+    return (
+        <h1>Hello About {message}</h1>
+    )
+}
 
 // when to use:
 // 1. bigger components
 // 2. lots of state
-class About extends React.Component {
-    render() {
-        return (
-            <h1>Hello I am class component</h1>
-        )
-    }
-}
+// class About extends React.Component {
+//     render() {
+//         return (
+//             <h1>Hello I am class component</h1>
+//         )
+//     }
+// }
 
 
 export default About
